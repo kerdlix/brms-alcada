571868aff78296638d3147738a9a74fbe74c19db
brmsAdmin <>
2017-09-01 11:44:24 -0300

    {/alcada/src/main/java/com/redhat/alcada/Grupo.java}

--- alcada/src/main/java/com/redhat/alcada/Grupo.java
+++ alcada/src/main/java/com/redhat/alcada/Grupo.java
@@ -11,6 +11,10 @@ public class Grupo implements java.io.Serializable
 
    private java.util.List<com.redhat.alcada.Elemento> elementos;
 
+   private com.redhat.alcada.Alcada _alcada1;
+
+   private com.redhat.alcada.Alcada _alcada2;
+
    public Grupo()
    {
    }
@@ -26,9 +30,32 @@ public class Grupo implements java.io.Serializable
       this.elementos = elementos;
    }
 
-   public Grupo(java.util.List<com.redhat.alcada.Elemento> elementos)
+   public com.redhat.alcada.Alcada get_alcada1()
+   {
+      return this._alcada1;
+   }
+
+   public void set_alcada1(com.redhat.alcada.Alcada _alcada1)
+   {
+      this._alcada1 = _alcada1;
+   }
+
+   public com.redhat.alcada.Alcada get_alcada2()
+   {
+      return this._alcada2;
+   }
+
+   public void set_alcada2(com.redhat.alcada.Alcada _alcada2)
+   {
+      this._alcada2 = _alcada2;
+   }
+
+   public Grupo(java.util.List<com.redhat.alcada.Elemento> elementos,
+         com.redhat.alcada.Alcada _alcada1, com.redhat.alcada.Alcada _alcada2)
    {
       this.elementos = elementos;
+      this._alcada1 = _alcada1;
+      this._alcada2 = _alcada2;
    }
 
 }
